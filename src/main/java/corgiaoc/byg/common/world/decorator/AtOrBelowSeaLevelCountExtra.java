bda6fc5e7bac1f9da29538bb5a3e2690cb1cc173
HeartlessAUS <47524314+HeartlessAUS@users.noreply.github.com>
2021-07-31 13:12:45 +1000

    Revert "Update AtOrBelowSeaLevelCountExtra.java"
    This reverts commit 0d5bcf158f52ee1458ed3ce330ddcb3c0b2bd677.

--- src/main/java/corgiaoc/byg/common/world/decorator/AtOrBelowSeaLevelCountExtra.java
+++ src/main/java/corgiaoc/byg/common/world/decorator/AtOrBelowSeaLevelCountExtra.java
@@ -2,22 +2,22 @@ package corgiaoc.byg.common.world.decorator;
 
 import com.mojang.serialization.Codec;
 import corgiaoc.byg.common.world.decorator.config.AtOrBelowSeaLevelCountExtraConfig;
-import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.gen.feature.WorldDecoratingHelper;
-import net.minecraft.world.gen.placement.Placement;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.DecoratorContext;
 
 import java.util.Random;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-public class AtOrBelowSeaLevelCountExtra extends Placement<AtOrBelowSeaLevelCountExtraConfig> {
+public class AtOrBelowSeaLevelCountExtra extends Decorator<AtOrBelowSeaLevelCountExtraConfig> {
 
     public AtOrBelowSeaLevelCountExtra(Codec<AtOrBelowSeaLevelCountExtraConfig> config) {
         super(config);
     }
 
-    public Stream<BlockPos> getPositions(WorldDecoratingHelper ctx, Random random, AtOrBelowSeaLevelCountExtraConfig config, BlockPos pos) {
+    public Stream<BlockPos> getPositions(DecoratorContext ctx, Random random, AtOrBelowSeaLevelCountExtraConfig config, BlockPos pos) {
         int repeats = config.count;
         if (random.nextFloat() < config.extraChance) {
             repeats += config.extraCount;
@@ -31,13 +31,13 @@ public class AtOrBelowSeaLevelCountExtra extends Placement<AtOrBelowSeaLevelCoun
 
             BlockPos.Mutable mutable = new BlockPos.Mutable(j, l, k);
             while (moveDown <= config.belowSeaLevel) {
-                if (ctx.getBlockState(mutable).canOcclude())
+                if (ctx.getBlockState(mutable).isOpaque())
                     break;
 
                 mutable.move(Direction.DOWN);
                 moveDown++;
             }
-            return mutable.immutable();
+            return mutable.toImmutable();
         });
     }
 }
